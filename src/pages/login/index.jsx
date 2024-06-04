a79abb0ce5e48dfd6ae312c623edba4092f97f3a
VigBon <ckube2033@gmail.com>
2024-06-05 01:00:47 +0700

    fix: reponsive login, register

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -11,13 +11,10 @@ import {
   IconButton,
   Button,
   Avatar,
-  Grid,
-  Container
 } from '@mui/material'
 import AppleIcon from '@mui/icons-material/Apple'
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye'
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Login() {
   const navigate = useNavigate()
@@ -65,12 +62,12 @@ function Login() {
         height="auto"
         alignItems={'center'}
         position={'absolute'}
-        top={'50%'}
         left={'50%'}
         textAlign={'center'}
-        sx={{ transform: 'translate(-50%, -50%)' }}
+        sx={{ transform: 'translateX(-50%)' }}
       >
         <Box
+          mt={"24px"}
           sx={{
             width: 'auto',
             height: '160px',
@@ -107,7 +104,7 @@ function Login() {
               variant="outlined"
               type="text"
               sx={{
-                width: '500px',
+                width: '100%',
                 backgroundColor: ' white',
                 borderRadius: '20px',
                 marginTop: '8px',
@@ -141,7 +138,7 @@ function Login() {
                 )
               }}
               sx={{
-                width: '500px',
+                width: '100%',
                 backgroundColor: ' white',
                 borderRadius: '20px',
                 marginTop: '8px',
@@ -216,6 +213,7 @@ function Login() {
             <Avatar alt="facebook" src="./public/facebook.svg" />
           </Box>
           <Box
+            mb={"24px"}
             sx = {{
               display: 'flex',
               justifyContent: 'center'
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,14 +1,39 @@
+import { useState, useEffect } from 'react'
+import { toast } from 'react-toastify'
+import { useNavigate, NavLink } from 'react-router-dom'
+import { loginApi } from '@/api/UserService'
 import {
+  InputAdornment,
   TextField,
   Box,
   Typography,
+  Link,
+  IconButton,
   Button,
-  Avatar
+  Avatar,
+  Grid,
+  Container
 } from '@mui/material'
 import AppleIcon from '@mui/icons-material/Apple'
-import { NavLink } from 'react-router-dom'
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Register() {
+  const navigate = useNavigate()
+
+  const [visible, setVisible] = useState(false)
+
+  const EndAdorment = ({ visible, setVisible }) => {
+    return (
+      <InputAdornment position="end">
+        <IconButton onClick={() => setVisible(!visible)}>
+          {visible ? <RemoveRedEyeIcon /> : <VisibilityOffIcon />}
+        </IconButton>
+      </InputAdornment>
+    )
+  }
+
   return (
     <Box width="100vw" height="100vh">
       <Box
@@ -18,9 +43,10 @@ function Register() {
         position={'absolute'}
         left={'50%'}
         textAlign={'center'}
-        sx={{ transform: 'translateX(-50%)'}}
+        sx={{ transform: 'translateX(-50%)' }}
       >
         <Box
+          mt={"24px"}
           sx={{
             width: 'auto',
             height: '160px',
@@ -38,189 +64,205 @@ function Register() {
           pl="30px"
           color={'#2EBAC1'}
         >
-            Monkey Blogging
+          Monkey Blogging
         </Typography>
-        {/* <form onSubmit={handleSubmit}> */}
 
         <Box marginTop={'24px'} textAlign={'left'}>
-          <Typography
-            variant="h4"
-            fontSize={'16px'}
-            fontWeight={'600'}
-            color={'#3C4563'}
-          >
-             Fullname
-          </Typography>
-          <TextField
-            placeholder=" Enter your fullname"
-            variant="outlined"
-            type="text"
-            sx={{
-              width: '500px',
-              backgroundColor: ' white',
-              borderRadius: '20px',
-              marginTop: '8px',
-              '& .css-7ohhq8-MuiInputBase-root-MuiOutlinedInput-root': {
-                borderRadius: '20px'
-              },
-              '.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
-                padding: '14px 14px'
-              }
-            }}
-          />
-        </Box>
-        <Box marginTop={'24px'} textAlign={'left'}>
-          <Typography
-            variant="h4"
-            fontSize={'16px'}
-            fontWeight={'600'}
-            color={'#3C4563'}
-          >
+            <Typography
+              variant="h4"
+              fontSize={'16px'}
+              fontWeight={'600'}
+              color={'#3C4563'}
+            >
+              Full name
+            </Typography>
+            <TextField
+              placeholder="Enter your full name"
+              variant="outlined"
+              type="text"
+              sx={{
+                width: '100%',
+                backgroundColor: ' white',
+                borderRadius: '20px',
+                marginTop: '8px',
+                '& .css-7ohhq8-MuiInputBase-root-MuiOutlinedInput-root': {
+                  borderRadius: '20px'
+                },
+                '.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
+                  padding: '14px 14px'
+                }
+              }}
+            />
+          </Box>
+
+          <Box marginTop={'24px'} textAlign={'left'}>
+            <Typography
+              variant="h4"
+              fontSize={'16px'}
+              fontWeight={'600'}
+              color={'#3C4563'}
+            >
               Email address
-          </Typography>
-          <TextField
-            placeholder="Enter your email address"
-            variant="outlined"
-            type="text"
-            sx={{
-              width: '500px',
-              backgroundColor: ' white',
-              borderRadius: '20px',
-              marginTop: '8px',
-              '& .css-7ohhq8-MuiInputBase-root-MuiOutlinedInput-root': {
-                borderRadius: '20px'
-              },
-              '.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
-                padding: '14px 14px'
-              }
-            }}
-          />
-        </Box>
-        <Box marginTop={'24px'} textAlign={'left'}>
-          <Typography
-            variant="h4"
-            fontSize={'16px'}
-            fontWeight={'600'}
-            color={'#3C4563'}
-          >
+            </Typography>
+            <TextField
+              placeholder="Enter your email address"
+              variant="outlined"
+              type="text"
+              sx={{
+                width: '100%',
+                backgroundColor: ' white',
+                borderRadius: '20px',
+                marginTop: '8px',
+                '& .css-7ohhq8-MuiInputBase-root-MuiOutlinedInput-root': {
+                  borderRadius: '20px'
+                },
+                '.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
+                  padding: '14px 14px'
+                }
+              }}
+            />
+          </Box>
+          <Box marginTop={'24px'} textAlign={'left'}>
+            <Typography
+              variant="h4"
+              fontSize={'16px'}
+              fontWeight={'600'}
+              color={'#3C4563'}
+            >
               Password
-          </Typography>
-          <TextField
-            placeholder="Enter your password"
-            variant="outlined"
-            type= 'text'
-            sx={{
-              width: '500px',
-              backgroundColor: ' white',
-              borderRadius: '20px',
-              marginTop: '8px',
-              '& .css-7ohhq8-MuiInputBase-root-MuiOutlinedInput-root': {
-                borderRadius: '20px'
-              },
-              '.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
-                padding: '14px 14px'
-              }
-            }}
-          />
-        </Box>
+            </Typography>
+            <TextField
+              placeholder="Enter your password"
+              variant="outlined"
+              type={visible ? 'text' : 'password'}
+              InputProps={{
+                endAdornment: (
+                  <EndAdorment visible={visible} setVisible={setVisible} />
+                )
+              }}
+              sx={{
+                width: '100%',
+                backgroundColor: ' white',
+                borderRadius: '20px',
+                marginTop: '8px',
+                '& .css-7ohhq8-MuiInputBase-root-MuiOutlinedInput-root': {
+                  borderRadius: '20px'
+                },
+                '.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
+                  padding: '14px 14px'
+                }
+              }}
+            />
+          </Box>
 
-        <Box marginTop={'24px'} textAlign={'left'}>
-          <Typography
-            variant="h4"
-            fontSize={'16px'}
-            fontWeight={'600'}
-            color={'#3C4563'}
-          >
-            Return password
-          </Typography>
-          <TextField
-            placeholder="Enter your password"
-            variant="outlined"
-            type= 'text'
-            sx={{
-              width: '500px',
-              backgroundColor: ' white',
-              borderRadius: '20px',
-              marginTop: '8px',
-              '& .css-7ohhq8-MuiInputBase-root-MuiOutlinedInput-root': {
-                borderRadius: '20px'
-              },
-              '.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
-                padding: '14px 14px'
-              }
-            }}
-          />
-        </Box>
+          <Box marginTop={'24px'} textAlign={'left'}>
+            <Typography
+              variant="h4"
+              fontSize={'16px'}
+              fontWeight={'600'}
+              color={'#3C4563'}
+            >
+              Return password
+            </Typography>
+            <TextField
+              placeholder="Enter your return password"
+              variant="outlined"
+              type={visible ? 'text' : 'password'}
+              InputProps={{
+                endAdornment: (
+                  <EndAdorment visible={visible} setVisible={setVisible} />
+                )
+              }}
+              sx={{
+                width: '100%',
+                backgroundColor: ' white',
+                borderRadius: '20px',
+                marginTop: '8px',
+                '& .css-7ohhq8-MuiInputBase-root-MuiOutlinedInput-root': {
+                  borderRadius: '20px'
+                },
+                '.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
+                  padding: '14px 14px'
+                }
+              }}
+            />
+          </Box>
 
-        <Box
-          display={'flex'}
-          alignItems={'center'}
-          justifyContent={'space-around'}
-          width={'80%'}
-          margin={'8px 0 8px 10%'}
-        >
-          <Button
-            variant="contained"
-            color="info"
-            sx={{
-              margin: '12px 0',
-              width: '200px',
-              lineHeight: '40px',
-              background: 'linear-gradient(to right, #00A7B4, #A4D96C)'
-            }}
+          <Box
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'space-around'}
+            width={'80%'}
+            margin={'8px 0 8px 10%'}
           >
-              Sign In
-          </Button>
-        </Box>
-        <Box margin={'16px 0'}>
-          <Box margin={'0 auto'} width={'100%'} border={'1px solid #ccc'}></Box>
-        </Box>
-        <Box
-          display={'flex'}
-          alignItems={'center'}
-          justifyContent={'center'}
-          marginBottom={'16px'}
-        >
-          <AppleIcon sx={{ fontSize:'50px' }}/>
-          <Avatar
-            sx={{
-              margin: '0 20px'
-            }}
-            alt="google"
-            src="./public/google.svg"
-          />
-          <Avatar alt="facebook" src="./public/facebook.svg" />
-        </Box>
-        <Box
-          sx = {{
-            display: 'flex',
-            justifyContent: 'center'
-          }}
-        >
-          <Typography
-            fontSize={'16px'}
-            fontWeight={'500'}
-            color={'#263238CC'}
-            sx = {{ marginRight: '4px' }}
+            <Button
+              variant="contained"
+              color="info"
+              type="submit"
+              sx={{
+                margin: '12px 0',
+                width: '200px',
+                lineHeight: '40px',
+                background: 'linear-gradient(to right, #00A7B4, #A4D96C)'
+              }}
+            >
+              Sign Up
+            </Button>
+          </Box>
+
+          <Box margin={'16px 0'}>
+            <Box
+              margin={'0 auto'}
+              width={'100%'}
+              border={'1px solid #ccc'}
+            ></Box>
+          </Box>
+          <Box
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            marginBottom={'16px'}
           >
-                You have an account,
-          </Typography>
-          <NavLink
-            to = "/login"
-            style={{
-              textDecoration: 'none'
+            <AppleIcon sx={{ fontSize:'50px' }}/>
+            <Avatar
+              sx={{
+                margin: '0 20px'
+              }}
+              alt="google"
+              src="./public/google.svg"
+            />
+            <Avatar alt="facebook" src="./public/facebook.svg" />
+          </Box>
+          <Box
+            mb={"24px"}
+            sx = {{
+              display: 'flex',
+              justifyContent: 'center'
             }}
           >
             <Typography
               fontSize={'16px'}
               fontWeight={'500'}
-              color={'#A4D96C'}
+              color={'#263238CC'}
+              sx = {{ marginRight: '6px' }}
             >
-              Sign in.
+                You have a account?
             </Typography>
-          </NavLink>
-        </Box>
-        {/* </form> */}
+            <NavLink
+              to = "/login"
+              style={{
+                textDecoration: 'none'
+              }}
+            >
+              <Typography
+                fontSize={'16px'}
+                fontWeight={'500'}
+                color={'#A4D96C'}
+              >
+              Sign in Now!
+              </Typography>
+            </NavLink>
+          </Box>
       </Box>
     </Box>
   )
